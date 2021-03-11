011d553ef030611439bb00ee7778044400aea09e
Charles Ledvina <charles@indexdata.com>
2021-03-11 10:38:19 -0600

    Added comments

--- actions/changeFundCodeByPoLineId.js
+++ actions/changeFundCodeByPoLineId.js
@@ -1,3 +1,18 @@
+/* 
+If status is "Open"
+
+1. GET orders/composite-orders/{poId}
+
+2. Change workflowStatus to "Pending"
+   PUT orders/composite-orders/{poId}
+
+3. Change compositePoLines[0].fundDistribution.fundId to new Fund ID.
+   Change compositePoLines[0].fundDistribution.code to new Fund code
+   Change workflowStatus to "Open"
+   PUT orders/composite-orders/{poId}
+*/
+
+
 metadata = {
   endpoint: 'orders/order-lines'
 };
