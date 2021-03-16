b3559284b8af75bdebcb84e9ef5928045f409170
Charles Ledvina <charles@indexdata.com>
2021-03-16 15:56:34 -0500

    Use orders-storage endpoint

--- actions/changeFundCodeByPoLineId.js
+++ actions/changeFundCodeByPoLineId.js
@@ -19,22 +19,31 @@ const metadata = {
 };
 
 const action = async (id, steps) => {
+  // get po line
   let lineUrl = `orders/order-lines/${id}`
   let poLine = await steps.goto(lineUrl);
-  let poUrl = `orders/composite-orders/${poLine.purchaseOrderId}`;
-  let po = await steps.goto(poUrl);
-  po.compositePoLines.forEach(c => {
-    if (c.id == id) {
-      c.fundDistribution.forEach(f => {
-        f.fundId = metadata.newFundId;
-        f.code = metadata.newFundCode;
-      });
-    }
+  // change po line
+  poLine.fundDistribution.forEach(f => {
+    f.fundId = metadata.newFundId;
+    f.code = metadata.newFundCode;
   });
-  steps.preview(po);
-  let previousStatus = po.workflowStatus;
+  // view change
+  steps.preview(poLine);
+  
+  // get order
+  let poUrl = `orders-storage/purchase-orders/${poLine.purchaseOrderId}`;
+  let po = await steps.goto(poUrl);
+  // unopen order
   po.workflowStatus = 'Pending';
+  // view status change
+  steps.preview(po);
+  // put changes
   await steps.send(poUrl, po);
+
+  // put changes to order line
+  await steps.send(lineUrl, poLine);
+
+  // open order
   po.workflowStatus = 'Open';
   await steps.send(poUrl, po);
   return;
