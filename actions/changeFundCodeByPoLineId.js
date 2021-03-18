9554e9bed12f1fd85b5e609b757306f5b6812a23
Charles Ledvina <charles@indexdata.com>
2021-03-18 16:56:10 -0500

    Better (but not much faster) fund change.

--- actions/changeFundCodeByPoLineId.js
+++ actions/changeFundCodeByPoLineId.js
@@ -20,28 +20,29 @@ const metadata = {
 
 const action = async (id, steps) => {
   // get po line
-  let lineUrl = `orders/order-lines/${id}`
+  let lineUrl = `orders-storage/po-lines/${id}`
   let poLine = await steps.goto(lineUrl);
-  // change po line
-  poLine.fundDistribution.forEach(f => {
-    f.fundId = metadata.newFundId;
-    f.code = metadata.newFundCode;
-  });
-  // view change
-  steps.preview(poLine);
   
   // get order
-  let poUrl = `orders-storage/purchase-orders/${poLine.purchaseOrderId}`;
+  poUrl = `orders/composite-orders/${poLine.purchaseOrderId}`;
   let po = await steps.goto(poUrl);
+
+  // change po line in composite order
+  po.compositePoLines.forEach(c => {
+    if (c.id === id) {
+      c.fundDistribution.forEach(f => {
+        f.fundId = metadata.newFundId;
+        f.code = metadata.newFundCode;
+      });
+    }
+  })
+
   // unopen order
   po.workflowStatus = 'Pending';
   // view status change
   steps.preview(po);
   // put changes
   await steps.send(poUrl, po);
-
-  // put changes to order line
-  await steps.send(lineUrl, poLine);
 
   // open order
   po.workflowStatus = 'Open';
