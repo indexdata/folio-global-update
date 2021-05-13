50e4519831d83c0ee1e7d363c7d005d4277d52fd
Charles Ledvina <charles@indexdata.com>
2021-05-13 13:23:22 -0500

    Add comment to action script.

--- actions/suppressInstances.js
+++ actions/suppressInstances.js
@@ -1,3 +1,9 @@
+/*
+  This action script will set the discoverySuppress property of an instance record to true.
+  Since this script targets the inventory/instances endpoint, FOLIO will set discoverySuppress
+  to true in the source record also.
+*/
+
 const action = async (id, steps) => {
   const url = `inventory/instances/${id}`;
   const record = await steps.goto(url);
