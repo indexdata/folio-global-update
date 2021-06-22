88e121fc5dccf5f4803053b6a27244773f18487c
Charles Ledvina <charles@folio-bastion.folio-dev.indexdata.com>
2021-06-22 19:00:51 +0000

    New action scripts to change item status be barcode.

---
+++ actions/changeItemStatusLost.js
@@ -0,0 +1,15 @@
+/*
+ Takes a list of barcodes and changes the item status to Declared lost.
+*/
+
+const action = async (id, steps) => {
+  let records = await steps.goto(`item-storage/items?query=barcode==${id}`);
+  let item = records.items[0];
+  // steps.term.log(item);
+  item.status.name = 'Declared lost';
+  // steps.term.log(item);
+  await steps.send(`item-storage/items/${item.id}`, item);
+  return;
+}
+
+module.exports = { action };
