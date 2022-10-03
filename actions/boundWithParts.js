4a444ed530c787d37cbc40762957cf4c5f570448
Charles Ledvina <charles@indexdata.com>
2022-10-03 15:58:22 -0500

    Search by holdings hrid instead of item hrid.

--- actions/boundWithParts.js
+++ actions/boundWithParts.js
@@ -2,14 +2,14 @@ const action = async (line, steps) => {
   if (line.match(/^\d/)) {
     let [ epn, bc, other ] = line.split(/\t/);
     const mainUrl = `item-storage/items?query=hrid==${epn}`;
-    const otherUrl = `item-storage/items?query=hrid==${other}`;
+    const otherUrl = `holdings-storage/holdings?query=hrid==${other}`;
     const mainRes = await steps.goto(mainUrl);
     const otherRes = await steps.goto(otherUrl);
-    if (mainRes.items[0] && otherRes.items[0]) {
+    if (mainRes.items[0] && otherRes.holdingsRecords[0]) {
       let mainItem = mainRes.items[0];
-      let otherItem = otherRes.items[0];
+      let otherItem = otherRes.holdingsRecords[0];
       let iid = mainItem.id;
-      let hid = otherItem.holdingsRecordId;
+      let hid = otherItem.id;
       let bwid = await steps.uuidgen(hid+iid);
       let bwObj = {
         id: bwid,
