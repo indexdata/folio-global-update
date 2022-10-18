2a5507aa354283e2fe79173b2b4fedd35ce40a9e
Charles Ledvina <charles@indexdata.com>
2022-10-18 14:32:19 -0500

    Add section to create bound-with-part object for main item.

--- actions/boundWithParts.js
+++ actions/boundWithParts.js
@@ -6,6 +6,17 @@ const action = async (line, steps) => {
     const otherUrl = `holdings-storage/holdings?query=hrid==${other}`;
     if (!mainRes) { 
       mainRes = await steps.goto(mainUrl);
+      if (mainRes.items[0]) {
+        let iid = mainRes.items[0].id;
+        let hid = mainRes.items[0].holdingsRecordId;
+        let bwid = await steps.uuidgen(hid + iid);
+        let bwObj = {
+          id: bwid,
+          holdingsRecordId: hid,
+          itemId: iid
+        }
+        await steps.post('inventory-storage/bound-with-parts', bwObj);
+      }
       await steps.putCache(epn, mainRes);
     }
     const otherRes = await steps.goto(otherUrl);
