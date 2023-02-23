a9eab5f6c1b602bc59d395ed645227fdc5494419
Charles Ledvina <charles@folio-bastion.folio-dev.indexdata.com>
2023-02-23 18:26:53 +0000

    Added comment.

--- actions/notLend2Lend.js
+++ actions/notLend2Lend.js
@@ -1,3 +1,9 @@
+/*
+ This script will search for "Will not lend" ILL policies in holdings records.  It will get a batch of 1000,
+ change, the ILL policy to "Will lend" and send the batch to holdings-storage.  It will read the
+ revolutions limit from a data file which should contain a single line with an integer.
+*/
+
 const action = async (line, steps) => {
 	const lim = parseInt(line, 10);
 	const willLend = '46970b40-918e-47a4-a45d-b1677a2d3d46';
