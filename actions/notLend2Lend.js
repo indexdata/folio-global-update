953319af76a804d6406c103d78c387e5847c15c1
Charles Ledvina <charles@folio-bastion.folio-dev.indexdata.com>
2023-02-23 18:21:39 +0000

    Read revs limit from data file. Add revs loop.

--- actions/notLend2Lend.js
+++ actions/notLend2Lend.js
@@ -1,16 +1,22 @@
 const action = async (line, steps) => {
+	const lim = parseInt(line, 10);
 	const willLend = '46970b40-918e-47a4-a45d-b1677a2d3d46';
 	const willNotLend = 'b0f97013-87f5-4bab-87f2-ac4a5191b489';
-	const postUrl = 'holdings-storage/batch/synchronous';
+	const postUrl = 'holdings-storage/batch/synchronous?upsert=true';
 	const searchUrl = `holdings-storage/holdings?limit=1000&query=illPolicyId==${willNotLend}&stats=false`
-	let hr = await steps.goto(searchUrl);
-	let ttl = hr.totalRecords;
-	steps.term.log(`Total records ${ttl}`);
-	hr.holdingsRecords.forEach(h => {
-		h.illPolicyId = willLend;
-	});
-	delete(hr.totalRecords);
-	await steps.post(postUrl, hr);
+	for (let x = 1; x <= lim; x++) {
+		steps.term.log(`Rev ${x} of ${lim}...`);
+		let hr = await steps.goto(searchUrl);
+		let ttl = hr.totalRecords;
+		if (!ttl) break;
+		steps.term.log(`Total records ${ttl}`);
+		hr.holdingsRecords.forEach(h => {
+			h.illPolicyId = willLend;
+		});
+		delete(hr.totalRecords);
+		delete(hr.resultInfo);
+		await steps.post(postUrl, hr);
+	}
   	return;
 }
 
