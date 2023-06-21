6aa71b173292a74591e3c06356a99f568576a916
Charles Ledvina <charles@folio-bastion.folio-us-east-1-1.hosted-folio.indexdata.com>
2023-06-21 15:20:25 +0000

    New action for adding circ note ids to items.

---
+++ actions/addCircNoteId.js
@@ -0,0 +1,21 @@
+/*
+ Takes a list of barcodes and changes the item status to Available.
+*/
+
+const action = async (id, steps) => {
+  let item = await steps.goto(`item-storage/items/${id}`);
+  // steps.term.log(item);
+  let cn = item.circulationNotes;
+  if (cn) {
+	for (x = 0; x < cn.length; x++) { 
+		let n = cn[x];
+		let ustr = `${id}--${x}`;
+		n.id = await steps.uuidgen(ustr);
+	}
+  }
+  // steps.term.log(item);
+  await steps.send(`item-storage/items/${item.id}`, item);
+  return;
+}
+
+module.exports = { action };
