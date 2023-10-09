122366dda66c97e2a798312f4804b5935e743cc7
Charles Ledvina <charles@folio-bastion.folio-us-east-1-1.hosted-folio.indexdata.com>
2023-10-09 21:30:08 +0000

    New script for updating sudoc MFHD records.

---
+++ actions/sudocFixMfhd.js
@@ -0,0 +1,29 @@
+const action = async (id, steps) => {
+  let ep = 'holdings-storage/holdings';
+  let rep = 'records-editor/records';
+  let cnType = 'fc388041-6cd0-4806-8a74-ebe3b9ab4c6e' // sudoc
+  let hr = JSON.parse(id);
+  let recs = await steps.goto(ep + '?query=hrid==' + hr.id);
+  let rec = (recs.holdingsRecords) ? recs.holdingsRecords[0] : '';
+  if (rec) {
+	  let murl = `${rep}?externalId=${rec.id}`;
+	  let mfhd = await steps.goto(murl);
+	  if (mfhd && mfhd.fields) {
+		  for (x = 0; x < mfhd.fields.length; x++) {
+			  let f = mfhd.fields[x];
+			  if (f.tag === '852') {
+				  f.content = f.content.replace(/ *\$[hi] [^$]+/, '');
+				  f.content += ` $h ${hr.cn}`;
+				  f.indicators[0] = '3';
+				  break;
+			  }
+	          }
+		  mfhd.relatedRecordVersion = `${rec._version}`;
+		  await steps.preview(mfhd);
+		  await steps.send(`${rep}/${mfhd.parsedRecordId}`, mfhd);
+          }
+  }
+  return;
+}
+
+module.exports = { action };
