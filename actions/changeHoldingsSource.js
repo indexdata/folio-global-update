2c25dfe43c7af8404c6269df09e4ec83c057d50e
Charles Ledvina <charles@folio-bastion.folio-us-east-1-1.hosted-folio.indexdata.com>
2023-10-20 17:27:08 +0000

    Add comment.

--- actions/changeHoldingsSource.js
+++ actions/changeHoldingsSource.js
@@ -1,3 +1,9 @@
+/*
+ * This script will change a holdings record sourceId to FOLIO 
+ * It will take a list of holdings IDs as input.
+*/
+
+
 const action = async (id, steps) => {
   let ep = 'holdings-storage/holdings';
   let sourceId = 'f32d531e-df79-46b3-8932-cdd35f7a2264' // FOLIO
