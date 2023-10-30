e6a05e1931039c0707b28108d9d751a24ca7d3eb
Charles Ledvina <charles@folio-bastion.folio-us-east-1-1.hosted-folio.indexdata.com>
2023-10-30 14:17:29 +0000

    Support hrid search as well.

--- actions/changeHoldingsSource.js
+++ actions/changeHoldingsSource.js
@@ -1,19 +1,25 @@
 /*
  * This script will change a holdings record sourceId to FOLIO 
- * It will take a list of holdings IDs as input.
+ * It will take a list of holdings IDs or hrids as input.
 */
 
 
 const action = async (id, steps) => {
-  let ep = 'holdings-storage/holdings';
-  let sourceId = 'f32d531e-df79-46b3-8932-cdd35f7a2264' // FOLIO
-  let rec = await steps.goto(ep + '/' + id);
-  if (rec) {
-	 rec.sourceId = sourceId;
-	 await steps.preview(rec);
-  	 await steps.send(ep + '/' + id, rec);
-  }
-  return;
+	let ep = 'holdings-storage/holdings';
+	let sourceId = 'f32d531e-df79-46b3-8932-cdd35f7a2264' // FOLIO
+	let rec = '';
+	if (id.match(/........-....-....-....-............/)) {
+	 	rec = await steps.goto(ep + '/' + id);
+	} else {
+		let recs = await steps.goto(ep + '?query=hrid==' + id);
+		rec = recs.holdingsRecords[0];
+	}
+	if (rec) {
+		rec.sourceId = sourceId;
+		await steps.preview(rec);
+		await steps.send(ep + '/' + rec.id, rec);
+	}
+	return;
 }
 
 module.exports = { action };
