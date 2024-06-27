60d3487bf158af26e9376f28650c9b87d518afa5
Charles Ledvina <charles@folio-bastion.folio-us-east-1-1.hosted-folio.indexdata.com>
2024-06-27 15:57:06 +0000

    New action script for deleting permanentLocation property.

---
+++ actions/delHoldingsPermLoc.js
@@ -0,0 +1,19 @@
+/*
+ * This script will delete the superfluous permanentLocation property from
+ * holdings records.
+*/
+
+const action = async (line, steps) => {
+	let hr = JSON.parse(line);
+	let id = hr.id;
+	const url = `holdings-storage/holdings/${id}`;
+	const record = await steps.goto(url);
+	if (record.permanentLocation) {
+		delete record.permanentLocation;
+		steps.preview(record);
+		await steps.send(url, record);
+	}
+	return;
+}
+
+module.exports = { action };
