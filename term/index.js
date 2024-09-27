22c85b4aeee949b315d04c34f8bb16c6691cd1f6
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2024-09-27 14:43:51 -0500

    Add viewCrud, get and put functions.

--- term/index.js
+++ term/index.js
@@ -7,9 +7,9 @@ let pageSize = 24;
 
 const mods = ['Users', 'Inventory'];
 const userSettings = {
-  'Permission sets': 'perm/permissions?query=mutable==true',
+  'Permission sets': 'perms/permissions?query=mutable==true',
   'Patron groups': 'groups',
-  'Address types': 'address-types'
+  'Address types': 'addresstypes'
 }
 
 let confs = fs.readdirSync(confDir);
@@ -26,6 +26,24 @@ function exit() {
   console.log('See ya later...');
 };
 
+async function put(ep, payload) {
+  ep = ep.replace(/^\//, '');
+  let url = okapi + '/' + ep;
+  console.log(`PUT ${url}`);
+  try {
+    let res = await superagent
+    .put(url)
+    .send(payload)
+    .set('x-okapi-token', token)
+    .set('accept', '*/*')
+    .set('content-type', 'application/json');
+    return res;
+  } catch(e) {
+    let msg = (e.response) ? e.response.text : e;
+    return { err: e };
+  }
+}
+
 async function post(ep, payload, rtype) {
   if (!rtype) rtype = 'body';
   ep = ep.replace(/^\//, '');
@@ -39,6 +57,23 @@ async function post(ep, payload, rtype) {
     .set('accept', '*/*')
     .set('content-type', 'application/json');
     return res[rtype];
+  } catch(e) {
+    let msg = (e.response) ? e.response.text : e;
+    return { err: e };
+  }
+}
+
+async function get(ep) {
+  ep = ep.replace(/^\//, '');
+  let url = okapi + '/' + ep;
+  console.log(`GET ${url}`);
+  try {
+    let res = await superagent
+    .get(url)
+    .set ('x-okapi-token', token)
+    .set('accept', '*/*')
+    .set('content-type', 'application/json');
+    return res.body;
   } catch(e) {
     let msg = (e.response) ? e.response.text : e;
     return { err: e };
@@ -64,14 +99,94 @@ async function login(conf) {
       let cooks = res['set-cookie'];
       for (let x = 0; x < cooks.length; x++) {
         let c = cooks[x];
-        if (c.match(/^folioRefreshToken/)) {
-          token = c.replace(/^folioRefreshToken=([^;]+).*/, '$1');
+        if (c.match(/^folioAccessToken/)) {
+          token = c.replace(/^folioAccessToken=([^;]+).*/, '$1');
         }
       }
     }
     console.log('Login successful!');
     chooseMods();
   }
+}
+
+function edit(text, ep) {
+  inquirer
+  .prompt([
+    {type: 'editor', name: 'newText', message: 'Edit this', default: text},
+    {type: 'confirm', name: 'save', message: 'Save changes', default: true},
+  ])
+  .then((a) => {
+    if (a.save) {
+     put(ep, a.newText);
+    }
+  })
+  .catch((e) => {
+    let msg = e.message || e;
+    console.log(msg);
+  });  
+}
+
+async function viewCrud(ep) {
+  let rec = await get(ep);
+  let recStr = JSON.stringify(rec, null, 2);
+  console.log(recStr);
+
+  let ch = [new inquirer.Separator(), 'Edit', '<--'];
+  inquirer
+  .prompt([
+    {type: 'list', name: 'act', message: 'Choose one', choices: ch, pageSize: pageSize},
+  ])
+  .then((a) => {
+    if (a.act === '<--') {
+      settings();
+    } else {
+      edit(recStr, ep);
+    }
+  })
+  .catch((e) => {
+    let msg = e.message || e;
+    console.log(msg);
+  }); 
+
+}
+
+async function listCrud(ep) {
+  let brief = [];
+  let propName = '';
+  let link = ep.replace(/^(.+)\?.*/, '$1');
+  const res = await get(ep);
+  for (let prop in res) {
+    let p = res[prop];
+    if (Array.isArray(p)) {
+      for (let x = 0; x < p.length; x++) {
+        let r = p[x];
+        let l = r.name || r.code || r.group || r.displayName || r.permissionName || r.addressType;
+        let h = {
+          name: l,
+          value: link + '/' + r.id
+        }
+        brief.push(h);
+      }
+      propName = prop;
+      break;
+    }
+  }
+  brief.push(new inquirer.Separator(), '<--');
+  inquirer
+  .prompt([
+    {type: 'list', name: 'ep', message: `Settings ${propName}`, choices: brief, pageSize: pageSize},
+  ])
+  .then((a) => {
+    if (a.ep === '<--') {
+      settings();
+    } else {
+      viewCrud(a.ep);
+    }
+  })
+  .catch((e) => {
+    let msg = e.message || e;
+    console.log(msg);
+  }); 
 }
 
 function userSet() {
@@ -84,15 +199,12 @@ function userSet() {
   .prompt([
     {type: 'list', name: 'set', message: 'User settings', choices: menu, pageSize: pageSize},
   ])
-  .then((a) => {
+  .then(async (a) => {
     if (a.mod === 'Cancel') {
       settings();
     } else {
-      let ep;
-      if (a.set === 'Permission sets') {
-        ep = userSettings[a.set];
-        console.log(ep);
-      }
+      let ep = userSettings[a.set];
+      listCrud(ep);
     }
   })
   .catch((e) => {
