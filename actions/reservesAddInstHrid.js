ad9b268766aa4f8958eecc0bf7527110617f244e
Charles Ledvina <charles@folio-bastion.folio-us-east-1-1.hosted-folio.indexdata.com>
2026-01-22 19:46:45 +0000

    New action to add instanct hrid to reserve items.

---
+++ actions/reservesAddInstHrid.js
@@ -0,0 +1,21 @@
+/*
+ Given a file of reserve object, this script will search for an instance by instanceId and 
+ add the instanceHrid to the reserve object and then PUT it.
+*/
+
+const action = async (line, steps) => {
+  let rr = JSON.parse(line);
+  let orig = JSON.parse(line);
+  if (!rr.instanceHrid) {
+  	let iid = rr.copiedItem.instanceId;
+  	let inst = await steps.goto(`instance-storage/instances/${iid}`);
+	if (inst) {
+		rr.copiedItem.instanceHrid = inst.hrid
+		await steps.send(`coursereserves/reserves/${rr.id}`, rr);
+		await steps.preview(rr, orig);
+	}
+  }
+  return;
+}
+
+module.exports = { action };
