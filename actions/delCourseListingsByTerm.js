79961059a77fd2e482d00a21040c1aae339b7d6a
Charles Ledvina <charles@folio-bastion.folio-us-east-1-1.hosted-folio.indexdata.com>
2026-09-14 20:46:14 +0000

    Delete any attached reserves or courses along with the term itself

--- actions/delCourseListingsByTerm.js
+++ actions/delCourseListingsByTerm.js
@@ -20,6 +20,8 @@ const action = async (line, steps) => {
 		let ts = await steps.goto(`coursereserves/courselistings?query=termId==${id}&limit=1000`);
 		let c = 0;
 		let ic = 0;
+		let cc = 0;
+		let rc = 0;
 		for (let x = 0; x < ts.courseListings.length; x++) {
 			let lid = ts.courseListings[x].id;
 			let lur = `coursereserves/courselistings/${lid}`;
@@ -30,10 +32,25 @@ const action = async (line, steps) => {
 				await steps.delete(iurl);
 				ic++;
 			}
+			let courses = await steps.goto(`coursereserves/courses?query=courseListingId==${lid}`)
+			for (let y = 0; y < courses.courses.length; y++) {
+				let courseId = courses.courses[y].id
+				let curl = `coursereserves/courses/${courseId}`;
+				await steps.delete(curl);
+				cc++;
+			}
+			let res = await steps.goto(`coursereserves/reserves?query=courseListingId==${lid}`)
+			for (let y = 0; y < res.reserves.length; y++) {
+				let resId = res.reserves[y].id
+				let rurl = `coursereserves/reserves/${resId}`;
+				await steps.delete(rurl);
+				rc++;
+			}
 			await steps.delete(lur);
 			c++;
 		}
-		await steps.term.log(`  INFO ${c} course listings with ${ic} instructors deleted`);
+		await steps.delete(`coursereserves/terms/${id}`);
+		await steps.term.log(`  INFO ${c} course listings with ${ic} instructors, ${cc} courses and ${ic} reserves deleted`);
 	}
 	return;
 }
